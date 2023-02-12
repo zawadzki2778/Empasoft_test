87e16ce968799435b08979dfdb49d49ea37f0c2c
Maksim Zawadzki <zawadzki2778@gmail.com>
2023-02-12 14:10:02 +0300

    created method to get data from fake server

--- src/pages/UserTable.vue
+++ src/pages/UserTable.vue
@@ -195,42 +195,13 @@
 </template>
 
 <script>
+// import { mapActions, mapGetters } from "vuex";
 export default {
   name: "UserTable",
   data() {
     return {
-      items: [
-        {
-          isActive: true,
-          age: 40,
-          name: { first: "Dickerson", last: "Macdonald" },
-        },
-        { isActive: false, age: 21, name: { first: "Larsen", last: "Shaw" } },
-        {
-          isActive: false,
-          age: 9,
-          name: { first: "Mini", last: "Navarro" },
-          _rowVariant: "success",
-        },
-        { isActive: false, age: 89, name: { first: "Geneva", last: "Wilson" } },
-        { isActive: true, age: 38, name: { first: "Jami", last: "Carney" } },
-        { isActive: false, age: 27, name: { first: "Essie", last: "Dunlap" } },
-        { isActive: true, age: 40, name: { first: "Thor", last: "Macdonald" } },
-        {
-          isActive: true,
-          age: 87,
-          name: { first: "Larsen", last: "Shaw" },
-          _cellVariants: { age: "danger", isActive: "warning" },
-        },
-        { isActive: false, age: 26, name: { first: "Mitzi", last: "Navarro" } },
-        {
-          isActive: false,
-          age: 22,
-          name: { first: "Genevieve", last: "Wilson" },
-        },
-        { isActive: true, age: 38, name: { first: "John", last: "Carney" } },
-        { isActive: false, age: 29, name: { first: "Dick", last: "Dunlap" } },
-      ],
+      items: [],
+      errors: [],
       fields: [
         {
           key: "name",
@@ -247,7 +218,8 @@ export default {
         {
           key: "isActive",
           label: "Is Active",
-          formatter: (value) => { // убрал  key, item
+          formatter: (value) => {
+            // убрал  key, item
             return value ? "Yes" : "No";
           },
           sortable: true,
@@ -273,6 +245,9 @@ export default {
     };
   },
   computed: {
+    // // ************ from VUEX ************ //
+    // ...mapGetters(["GET_ITEMS"]),
+
     sortOptions() {
       // Create an options list from our fields
       return this.fields
@@ -285,8 +260,32 @@ export default {
   mounted() {
     // Set the initial number of items
     this.totalRows = this.items.length;
+
+    this.getUserData();
+    //  // ************ from VUEX ************ //
+    //   this.GET_ITEMS(this.items);
+
+    //   this.FETCH_ITEMS(this.items);
   },
   methods: {
+    // // ************ from VUEX ************ //
+    // ...mapActions(["FETCH_ITEMS"]),
+    async getUserData() {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+      try {
+        this.items = await response.json();
+        console.log(this.items);
+
+        alert("ALL OK!");
+        
+      } catch (errors) {
+        this.errors = await response.json();
+        console.log(this.errors);
+      }
+    },
+
     info(item, index, button) {
       this.infoModal.title = `Row index: ${index}`;
       this.infoModal.content = JSON.stringify(item, null, 2);
